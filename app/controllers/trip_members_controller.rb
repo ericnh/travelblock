be824912d41ebb451eb862e187443658a971bcad
eric <halstead.eric@gmail.com>
2017-04-09 11:25:20 -0700

    inline deletion for trip members

--- app/controllers/trip_members_controller.rb
+++ app/controllers/trip_members_controller.rb
@@ -1,10 +1,25 @@
 class TripMembersController < ApplicationController
+  before_action :set_trip_member, only: [:destroy]
+  before_action :authenticate_user!
+
   def new
     @trip = Trip.find(params[:trip_id])
   end
 
   def create
-    @trip_member = TripMember.find_or_create_by(user_id: params[:user_id], trip_id: params[:trip_id])
+    @trip_member = TripMember.find_by(user_id: params[:user_id], trip_id: params[:trip_id])
+    @trip_member ||= TripMember.create(user_id: params[:user_id], trip_id: params[:trip_id])
     render json: @trip_member
+  end
+
+  def destroy
+    @trip_member.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_trip_member
+    @trip_member = TripMember.find(params[:id])
   end
 end
