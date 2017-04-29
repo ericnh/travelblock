f917225e750a3b19df9f3f24b130282c9ab04bd0
eric <halstead.eric@gmail.com>
2017-04-29 16:35:17 -0700

    I changed my mind

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170409190454) do
+ActiveRecord::Schema.define(version: 20170429233152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20170409190454) do
     t.integer  "trip_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "body"
     t.index ["trip_id"], name: "index_discussions_on_trip_id", using: :btree
   end
 
